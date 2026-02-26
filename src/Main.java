62cc7c1cedf743d17ad92945d39d1942561228d7
mesak <mrai3826@zenithacademy.org>
2026-02-26 11:21:17 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-int
-}
+        int numToExamine = 2;
+
+        if (numToExamine % 2 == 0)
+            System.out.println(numToExamine + " is Even.");
+        else
+            System.out.println(numToExamine + " is Odd.");
+
+    }}
